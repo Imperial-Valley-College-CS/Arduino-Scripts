f60d267b3e95fc0575e55ddf1dcbd7b125b7434a
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-10 12:30:49 -0700

    Update BinaryLEDS_UserInput.ino

--- Fall 2025/BinaryLEDS_UserInput/BinaryLEDS_UserInput.ino
+++ Fall 2025/BinaryLEDS_UserInput/BinaryLEDS_UserInput.ino
@@ -1,8 +1,13 @@
 int num;
+int leds[] = {2,3,4,5};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  for(int i = 0; i < sizeof(leds); i++)
+  {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
 void loop() {
@@ -10,5 +15,18 @@ void loop() {
   if( Serial.available() > 0 )
   {
     num = Serial.parseInt();
+    int i = 0;
+    while( num > 0 )
+    {
+      if( num % 2 == 1 )
+      {
+        digitalWrite(leds[i], HIGH);
+      }else
+      {
+        digitalWrite(leds[i], LOW);
+      }
+      num = num/2;
+      i++;
+    }
   }//end if
 }//end loop
