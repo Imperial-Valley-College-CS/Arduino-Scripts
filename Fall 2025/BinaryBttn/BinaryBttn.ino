3d30ebffea6a245eed8e62e20ab64e3bc474a0f8
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-15 12:13:01 -0700

    Update BinaryBttn.ino

--- Fall 2025/BinaryBttn/BinaryBttn.ino
+++ Fall 2025/BinaryBttn/BinaryBttn.ino
@@ -1,10 +1,15 @@
 int bttnPin = 4;
 int bttnValue = 0;
+int leds[] = {2,3,4,5};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(bttnPin, INPUT);
+  for(int i = 0; i < sizeof(leds); i++)
+  {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
 void loop() {
@@ -22,5 +27,17 @@ void loop() {
 
 void dispBinary( int num )
 {
-  
+  int i = 0;
+  while( num > 0 )
+  {
+    if( num % 2 == 1 )
+    {
+      digitalWrite(leds[i], HIGH);
+    }else
+    {
+      digitalWrite(leds[i], LOW);
+    }
+    num = num/2;
+    i++;
+  }
 }
