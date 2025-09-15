72cdd77ad328712a38bae27c02c920f539325526
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-15 11:36:52 -0700

    Create BinaryBttn.ino

---
+++ Fall 2025/BinaryBttn/BinaryBttn.ino
@@ -0,0 +1,13 @@
+int bttPin = 4;
+int bttnValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(bttnPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
