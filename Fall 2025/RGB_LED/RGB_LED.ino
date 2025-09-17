5bdeaac1eb3a3f5818745dd14f9480d023eae9df
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-17 12:04:45 -0700

    Update RGB_LED.ino

--- Fall 2025/RGB_LED/RGB_LED.ino
+++ Fall 2025/RGB_LED/RGB_LED.ino
@@ -1,12 +1,6 @@
 int redPin = A0;
 int bluePin = A1;
 int greenPin = A2;
-int redSensor = A3;
-int greenSensor = A4;
-int blueSensor = A5;
-int redValue;
-int greenValue;
-int blueValue;
 
 void setup() {
   // put your setup code here, to run once:
