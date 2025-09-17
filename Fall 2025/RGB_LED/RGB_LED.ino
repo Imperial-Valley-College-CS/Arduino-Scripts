ab4bc57470bd765b955daab83b91f5f8840eddaf
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-17 11:35:35 -0700

    Update RGB_LED.ino

--- Fall 2025/RGB_LED/RGB_LED.ino
+++ Fall 2025/RGB_LED/RGB_LED.ino
@@ -11,6 +11,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(redPin, 255);
-  
+  setColor(255,0,0);
+  delay(500);
+  setColor(0,255,0);
+  delay(500);
+  setColor(0,0,255);
+  delay(500);
+}
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
 }
