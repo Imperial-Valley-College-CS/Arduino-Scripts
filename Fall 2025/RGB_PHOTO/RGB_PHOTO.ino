b0a4ec68f7879020ea679f8bd901253b38e99fed
ortoct <104336410+ortoct@users.noreply.github.com>
2025-09-17 12:04:27 -0700

    Create RGB_PHOTO.ino

---
+++ Fall 2025/RGB_PHOTO/RGB_PHOTO.ino
@@ -0,0 +1,24 @@
+int redPin = A0;
+int bluePin = A1;
+int greenPin = A2;
+int redSensor = A3;
+int greenSensor = A4;
+int blueSensor = A5;
+int redValue;
+int greenValue;
+int blueValue;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redSensor, INPUT);
+  pinMode(greenSensor, INPUT);
+  pinMode(blueSensor, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
