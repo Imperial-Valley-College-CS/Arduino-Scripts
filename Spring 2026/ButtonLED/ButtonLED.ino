314fbe2efe7cb5fe9b7b0f82177aeae816c22ebd
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-11 12:13:14 -0700

    Update ButtonLED.ino

--- Spring 2026/ButtonLED/ButtonLED.ino
+++ Spring 2026/ButtonLED/ButtonLED.ino
@@ -1,6 +1,7 @@
 int bttnVal;
 int ledPin = 13;
 int bttnPin = 2;
+int ledState = 0;   // off-0, on-1
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,9 +12,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   bttnVal = digitalRead(bttnPin);   //read voltage at bttPin
-  if( bttnVal == HIGH )
+  if( bttnVal == HIGH && ledState == 0 )
   {
-    blinkLED( ledPin, 1000 );
+    digitalWrite(ledPin, HIGH);
+    ledState = 1;
+  }else if(bttnVal == HIGH && ledState == 1)
+  {
+    digitalWrite(ledPin, LOW);
+    ledState = 0;
   }
 }
 
