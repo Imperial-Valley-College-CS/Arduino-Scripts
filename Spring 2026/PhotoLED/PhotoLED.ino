ec0594833efc6845fffb5f38f2f2a51a00f26000
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-16 12:01:18 -0700

    Update PhotoLED.ino

--- Spring 2026/PhotoLED/PhotoLED.ino
+++ Spring 2026/PhotoLED/PhotoLED.ino
@@ -1,15 +1,26 @@
 double sensorValue;
 int sensorPin = A0;
+double mid = 2.72;
+int ledPin = 13;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue = (5.0/1023.0)*analogRead(sensorPin);
+  if( sensorValue >= mid )
+  {
+    digitalWrite( ledPin, HIGH );
+  }else
+  {
+    digitalWrite( ledPin, LOW );
+  }
+  
   Serial.println( sensorValue );
   delay(500);
 }
