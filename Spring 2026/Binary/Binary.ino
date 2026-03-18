de3bfa57aff326cf7dde249eafa4dcdc6378b619
ortoct <104336410+ortoct@users.noreply.github.com>
2026-03-18 12:11:40 -0700

    Create Binary.ino

---
+++ Spring 2026/Binary/Binary.ino
@@ -0,0 +1,28 @@
+int ledPins[] = {13, 12, 11, 10};   //13-8 bit, 12-4 bit, 11-2 bit
+
+void setup() {
+  // put your setup code here, to run once:
+  for( int x = 0; x < sizeof(ledPins); x++ )
+  {
+    pinMode(ledPins[x], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+void displayBin( int num )
+{
+  int x = 0;
+  while( num > 0 )
+  {
+    if( num % 2 == 1 )
+    {
+      digitalWrite(ledPins[x], HIGH);
+    }
+    num = num/2;
+    x++;
+  }
+}
