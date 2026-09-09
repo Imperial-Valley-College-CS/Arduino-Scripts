37697b87b30fe04950d02f82c710095eb51f4394
ortoct <104336410+ortoct@users.noreply.github.com>
2026-09-09 10:51:46 -0700

    Update PhotoRes_Resistance.ino

--- Fall 2026/PhotoRes_Resistance/PhotoRes_Resistance.ino
+++ Fall 2026/PhotoRes_Resistance/PhotoRes_Resistance.ino
@@ -1,4 +1,7 @@
 double v2;
+const double R2 = 10000.0;
+const double Vs = 5.0;
+double photoRes;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,7 +10,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  v2 = analogRead(A0);
+  v2 = analogRead(A0)*5.0/1023.0;
   Serial.println(v2);
   delay(500);
 }
