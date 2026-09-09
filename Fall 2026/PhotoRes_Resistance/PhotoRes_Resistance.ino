2ca8928cce75aba19ad3410040669a0c9dfdf001
ortoct <104336410+ortoct@users.noreply.github.com>
2026-09-09 10:43:45 -0700

    Create PhotoRes_Resistance.ino

---
+++ Fall 2026/PhotoRes_Resistance/PhotoRes_Resistance.ino
@@ -0,0 +1,13 @@
+double v2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  v2 = analogRead(A0);
+  Serial.println(v2);
+  delay(500);
+}
