8ce3e8271cec15921035fe9d17c4c1a16ab5ac66
ortoct <104336410+ortoct@users.noreply.github.com>
2026-09-21 12:40:57 -0700

    Update RemoteAndFunctions.ino

--- Fall 2026/RemoteAndFunctions/RemoteAndFunctions.ino
+++ Fall 2026/RemoteAndFunctions/RemoteAndFunctions.ino
@@ -3,6 +3,9 @@
 #include <boarddefs.h>
 #include <ir_Lego_PF_BitStreamEncoder.h>
 
+#define ON_OFF_BTTN 0xFF629D
+#define A_BTTN 0xFF22DD
+
 int IRsensor = 11;
 IRrecv irrecv(IRsensor);
 decode_results results;
@@ -12,15 +15,27 @@ void setup() {
   irrecv.enableIRIn();    //start receiver
   pinMode(IRsensor, INPUT); //configure pin 11 for INPUT
   Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if( irrecv.decode(&results) )
   {
+      if( results.value == ON_OFF_BTTN )
+      {
+        controlRGB(255,0,0);
+      }
+      else if(results.value == A_BTTN)
+      {
+        controlRGB(0,255,0);
+      }
       Serial.println( results.value, HEX );
       irrecv.resume();
   }
+                     
   delay(300);
 }
 
