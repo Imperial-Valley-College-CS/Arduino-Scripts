6b3dc401df33c410032ed673c7f41e8d37a9f2b4
ortoct <104336410+ortoct@users.noreply.github.com>
2026-09-23 11:27:18 -0700

    Update RemoteAndFunctions.ino

--- Fall 2026/RemoteAndFunctions/RemoteAndFunctions.ino
+++ Fall 2026/RemoteAndFunctions/RemoteAndFunctions.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include <IRremote.h>
 #include <IRremoteInt.h>
 #include <boarddefs.h>
@@ -5,14 +6,19 @@
 
 #define ON_OFF_BTTN 0xFF629D
 #define A_BTTN 0xFF22DD
+#define UP_BTTN 0xFF9867
 
 int IRsensor = 11;
+int servoPin = 9;
+int pos = 0;
 IRrecv irrecv(IRsensor);
 decode_results results;
+Servo myservo;
 
 void setup() {
   // put your setup code here, to run once:
   irrecv.enableIRIn();    //start receiver
+  myservo.attach(servoPin);
   pinMode(IRsensor, INPUT); //configure pin 11 for INPUT
   Serial.begin(9600);
   pinMode(A0, OUTPUT);
@@ -28,9 +34,18 @@ void loop() {
       {
         controlRGB(255,0,0);
       }
-      else if(results.value == A_BTTN)
+      if(results.value == A_BTTN)
       {
         controlRGB(0,255,0);
+      }
+      if( results.value == UP_BTTN)
+      {
+        pos = pos+10;
+        if( pos > 180 )
+        {
+          pos = 180;
+        }
+        myservo.write(pos);
       }
       Serial.println( results.value, HEX );
       irrecv.resume();
